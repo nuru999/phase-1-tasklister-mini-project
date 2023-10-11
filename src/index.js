2444576b2adbe4fdbcd3e946b7def819e27efbeb
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-11 13:44:30 +0300

    completed assignments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here//
+  const taskList = document.querySelector("#tasks");
+  const taskForm = document.querySelector("#create-task-form");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    const newTaskDescription = document.querySelector("#new-task-description").value;
+    addNewTask(newTaskDescription);
+    event.target.reset(); // Clear the form
+});
+
+function addNewTask(task) {
+  const listItem = document.createElement("li");
+  listItem.innerText = task;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", function() {
+    listItem.remove();
+  });
+
+  listItem.appendChild(deleteButton);
+  taskList.appendChild(listItem);
+}
 });
